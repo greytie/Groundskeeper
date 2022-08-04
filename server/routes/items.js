2cc5f7548015089d1983ad5356986cd430ccbceb
UltraFutile <ultrafutile@protonmail.com>
2022-08-03 22:00:49 -0500

    setup basic express server

--- server/routes/items.js
+++ server/routes/items.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send([
+    'Paint fence',
+    'Pick herbs',
+    'Mow the lawn'
+  ]);
 });
 
 module.exports = router;
